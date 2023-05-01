febbb925475f2c850be93b48615abf9d142b0a86
Ned <23404711+nedredmond@users.noreply.github.com>
2023-05-01 10:42:16 -0400

    deploy script

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,6 +14,9 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
+		paths: {
+			base: dev ? '' : process.env.BASE_PATH,
+		},
 	},
 };
 
