2dec14e510a0b97b4f9912a376515b4a2113ada8
Ned <23404711+nedredmond@users.noreply.github.com>
2023-05-06 12:06:03 -0400

    allow undef for _

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -30,5 +30,14 @@ module.exports = {
 	],
 	rules: {
 		'@typescript-eslint/no-non-null-assertion': 'off',
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': [
+			'warn',
+			{
+				argsIgnorePattern: '^_',
+				varsIgnorePattern: '^_',
+				caughtErrorsIgnorePattern: '^_',
+			},
+		],
 	},
 };
